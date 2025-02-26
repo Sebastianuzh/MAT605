116287bcb521532915ca135f5ddfe37c904759e8
Sebastian Schwarzenbach <sebastian.schwarzenbach@uzh.ch>
2025-02-26 15:56:41 +0100

    änderungen mit grundlagen video

--- week_1.hs
+++ week_1.hs
@@ -5,3 +5,15 @@ doubleMe x = x * 2
 
 tripleMe :: Int -> Int
 tripleMe y = y * 3
+
+doubleUs :: Int -> Int -> Int
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber :: (Ord a, Num a) => a -> a
+doubleSmallNumber x = 
+    if x>100
+        then x 
+        else 2*x
+
+-- test
+
